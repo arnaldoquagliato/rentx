5a8034b99be1f30d20cc1b008a153b57c4f942e9
arnaldoquagliato <arnaldoquagliato@gmail.com>
2022-08-08 08:35:36 -0300

    configuracao typeorm e criacao de migration de create category

--- src/database/migrations/1659903286533-CreateCateogies.ts
+++ src/database/migrations/1659903286533-CreateCateogies.ts
@@ -1,7 +1,6 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class CreateCategories1658973238626 implements MigrationInterface {
-
+export class CreateCateogies1659903286533 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
@@ -14,6 +13,10 @@ export class CreateCategories1658973238626 implements MigrationInterface {
                     },
                     {
                         name: "name",
+                        type: "varchar",
+                    },
+                    {
+                        name: "description",
                         type: "varchar",
                     },
                     {
